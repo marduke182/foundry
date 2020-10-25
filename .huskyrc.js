8171c1281edac955a75cb1265f5cdd1a52c2af79
Jesús Quintana <jquintana@atlassian.com>
2020-10-25 15:40:05 +1100

    chore: remove precommit upload manifest

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,11 +1,6 @@
-const tasks = arr => arr.join(' && ')
-
 module.exports = {
-  'hooks': {
-    "commit-msg": "yarn commitlint -E HUSKY_GIT_PARAMS",
-    'pre-commit': tasks([
-      'yarn lint-staged',
-      'yarn gulp updateManifest',
-    ])
-  }
-}
+  hooks: {
+    'commit-msg': 'yarn commitlint -E HUSKY_GIT_PARAMS',
+    'pre-commit': 'yarn lint-staged',
+  },
+};
