cddf8604e76fed0dd22847517257cfc312df0ecb
Jesús Quintana <jquintana@atlassian.com>
2020-10-25 13:24:30 +1100

    chore: generate manifest each commit (partial solution to deploy)

---
+++ .huskyrc.js
@@ -0,0 +1,11 @@
+const tasks = arr => arr.join(' && ')
+
+module.exports = {
+  'hooks': {
+    "commit-msg": "yarn commitlint -E HUSKY_GIT_PARAMS",
+    'pre-commit': tasks([
+      'yarn lint-staged',
+      'yarn gulp updateManifest'
+    ])
+  }
+}
